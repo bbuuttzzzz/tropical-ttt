100648495f13a9f4e1933dc68987a953f2ef5ce1
kklluuttzz <pantsrain@gmail.com>
2020-10-13 21:36:21 -0500

    Initial Commit

---
+++ gamemode/mapvote/cl_mapvote.lua
@@ -0,0 +1,43 @@
+MAPTAG_COLORS = {
+  [MAPTAG_BARRICADE] = Color(70,200,200,255),
+  [MAPTAG_RUN_AND_GUN] = Color(70,70,200,255)
+}
+MAPTAG_GROUP_COLORS = {
+  [MAPTAGGROUP_SIZE] = Color(200,200,70,255),
+  [MAPTAGGROUP_MESH] = Color(200,200,200,255),
+  [MAPTAGGROUP_SCALE] = Color(200,70,70,255)
+}
+
+GM.MapTable = {}
+
+function GM:GetMapTagColor(tag)
+  return MAPTAG_COLORS[tag] or MAPTAG_GROUP_COLORS[self:GetMapTagGroup(tag)] or Color(255,0,255,255)
+end
+
+net.Receive("ter_map_table_init", function(length)
+  local table = net.ReadTable()
+
+  print("table received")
+
+  GAMEMODE.MapTable = table
+end)
+
+net.Receive("tter_votemap_start", function(length)
+	GAMEMODE:OpenMapVote()
+end)
+
+net.Receive("tter_votemap_update", function(length)
+  if not GAMEMODE.MapVotes then GAMEMODE.MapVotes = {} end
+
+  local updateCount = net.ReadUInt(8)
+
+  for n = 1, updateCount do
+    GAMEMODE.MapVotes[net.ReadString()] = net.ReadUInt(16)
+  end
+
+  GAMEMODE:UpdateMapVoteCounts()
+end)
+
+net.Receive("tter_forcemap_open", function(length)
+  GAMEMODE:OpenForceMap()
+end)
