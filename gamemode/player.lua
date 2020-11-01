8a3e41b83e57bcf4fb178b443f142badc3c0d85e
T0CKS1CK_NUNU <blakew9119@gmail.com>
2020-11-01 01:33:17 -0600

    More testing on statuses

--- gamemode/player.lua
+++ gamemode/player.lua
@@ -852,6 +852,9 @@ function GM:OnPlayerHitGround(ply, in_water, on_floater, speed)
          dmg:SetDamage(damage)
 
          ground:TakeDamageInfo(dmg)
+         if damage > 15 and ground:Health() > 0 then
+            ground:GiveTStatus("knockdown", damage * 0.075 )
+         end
       end
 
       -- our own falling damage is cushioned
@@ -871,6 +874,9 @@ function GM:OnPlayerHitGround(ply, in_water, on_floater, speed)
       -- play CS:S fall sound if we got somewhat significant damage
       if damage > 5 then
          sound.Play(table.Random(fallsounds), ply:GetShootPos(), 55 + math.Clamp(damage, 0, 50), 100)
+      end
+      if damage > 20 and ply:Health() > 0 then
+         ply:GiveTStatus("knockdown", damage * 0.05 )
       end
    end
 end
